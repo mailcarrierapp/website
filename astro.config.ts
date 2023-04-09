d36709f138b9d706a6742b8e20c6f9e203f04006
danilopolani <danilo.polani@gmail.com>
2023-04-09 18:34:28 +0200

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +14,9 @@ export default defineConfig({
     react(), // Algolia search bar
     preact(),
     tailwind(),
+    sitemap(),
   ],
-  site: `https://mailcarrier.app`,
+  site: 'https://mailcarrier.app',
   markdown: {
     rehypePlugins: [
       'rehype-slug',
